c3b56a22df0f631b91bf13c5f3a0d5b2410fa62d
AbuSohan <sohan@inflexionpointbd.com>
2022-02-10 13:53:56 +0600

    postgres db added

--- lavender/lavender/settings.py
+++ lavender/lavender/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'lavender.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lavender',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'PORT': '5432',
+        'HOST': 'localhost',
     }
 }
 
